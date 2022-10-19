86624a7d67e6e5f91d1ed124e6aa647461fce5ee
Bruno Athayde <83596254+Bugui86@users.noreply.github.com>
2022-10-19 09:01:28 -0300

    Add files via upload

---
+++ conversao_de_temperatura_3.py
@@ -0,0 +1,7 @@
+print(f'PROGRAMA PARA CONVERSÃO DE TEMPERATURA KELVIN PARA CELCIUS\n')
+
+kelvin = float(input('Digite a temperatura em Kelvin: '))
+
+celcius = round((kelvin - 273.15), 2)
+
+print(f'\nA temperatura digitada em Kelvin convertida em Celcius fica:{celcius}')
---
+++ conversao_de_temperatura_4.py
@@ -0,0 +1,7 @@
+print(f'PROGRAMA PARA LER UMA TEMPERATURA EM CELCIUS E CONVERTER PARA KELVIN\n')
+
+celcius = float(input('Digite a temperatura em Celcius: '))
+
+kelvin = round((celcius + 273.15), 2)
+
+print(f'\nA temperatura convertida em Kelvin fica: {kelvin}')
