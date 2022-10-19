8367058632c6e335d15fd1ea01ed9ee2f097977c
Bruno Athayde <83596254+Bugui86@users.noreply.github.com>
2022-10-19 15:49:31 -0300

    Update conversao_de_temperatura_2.py

--- conversao_de_temperatura_2.py
+++ conversao_de_temperatura_2.py
@@ -1,4 +1,4 @@
-print(f'PROGRAMA PARA CONVERTER A TEMPERATURA DE FAHRENHEIT PARA CELCIUS\n')
+print('PROGRAMA PARA CONVERTER A TEMPERATURA DE FAHRENHEIT PARA CELCIUS\n')
 
 fahrenheit = float(input('Informe a temperatura em Fahrenheit que deseja converter para Celcius: '))
 
