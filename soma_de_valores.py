a96249303465918c0dacb4e62433031053d40e77
Bruno Athayde <83596254+Bugui86@users.noreply.github.com>
2022-10-19 15:46:26 -0300

    Update soma_de_valores.py

--- soma_de_valores.py
+++ soma_de_valores.py
@@ -1,4 +1,4 @@
-print(f'PROGRAMA PARA LER A SOMA DE TRÊS VALORES INTEIROS\n')
+print('PROGRAMA PARA LER A SOMA DE TRÊS VALORES INTEIROS\n')
 
 num1 = (int(input('Digite o primeiro número: ')))
 num2 = (int(input('\nDigite o segundo número: ')))
